c58849dc76268360bf4863463453d990f1c1154d
NOEMOJI041 <aloksharma041@gmail.com>
2023-09-26 03:54:16 +0530

    clear code

--- ebot_nav2/scripts/ebot_nav_cmd.py
+++ ebot_nav2/scripts/ebot_nav_cmd.py
@@ -1,26 +1,8 @@
-#! /usr/bin/env python3
-# Copyright 2021 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from geometry_msgs.msg import PoseStamped
 from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 import rclpy
 from rclpy.duration import Duration
-
-"""
-Basic navigation demo to go to pose.
-"""
 
 
 def main():
@@ -28,7 +10,6 @@ def main():
 
     navigator = BasicNavigator()
 
-    # Set our demo's initial pose
     goal_pose1 = PoseStamped()
     goal_pose1.header.frame_id = 'map'
     goal_pose1.header.stamp = navigator.get_clock().now().to_msg()
@@ -49,26 +30,7 @@ def main():
     goal_pose3.pose.position.y = 2.5
     goal_pose3.pose.orientation.w = 1.57
 
-    # Activate navigation, if not autostarted. This should be called after setInitialPose()
-    # or this will initialize at the origin of the map and update the costmap with bogus readings.
-    # If autostart, you should `waitUntilNav2Active()` instead.
-    # navigator.lifecycleStartup()
-
-    # Wait for navigation to fully activate, since autostarting nav2
     navigator.waitUntilNav2Active()
-
-    # If desired, you can change or load the map as well
-    # navigator.changeMap('/path/to/map.yaml')
-
-    # You may use the navigator to clear or obtain costmaps
-    # navigator.clearAllCostmaps()  # also have clearLocalCostmap() and clearGlobalCostmap()
-    # global_costmap = navigator.getGlobalCostmap()
-    # local_costmap = navigator.getLocalCostmap()
-
-    # Go to our demos first goal pose
-
-    # sanity check a valid path exists
-    # path = navigator.getPath(initial_pose, goal_pose)
 
     navigator.goToPose(goal_pose1)
 
