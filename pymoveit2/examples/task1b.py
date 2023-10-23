92346c258e289633bea80bff6fae14569f97c755
noemoji041 <aloksharma041@gmail.com>
2023-10-24 02:32:48 +0530

    task2A 1 box reached

--- pymoveit2/examples/task1b.py
+++ pymoveit2/examples/task1b.py
@@ -21,6 +21,7 @@ from tf_transformations import quaternion_from_euler
 import tf_transformations
 from geometry_msgs.msg import TwistStamped
 from linkattacher_msgs.srv import AttachLink
+from linkattacher_msgs.srv import DetachLink
 from math import cos, sin
 import math, time
 from copy import deepcopy
@@ -68,6 +69,8 @@ def main():
 	enftf = endf()
 	twist_pub = enftf.create_publisher(TwistStamped, "/servo_node/delta_twist_cmds", 10)
 	gripper_control = enftf.create_client(AttachLink, '/GripperMagnetON')
+	gripper_control_off = enftf.create_client(DetachLink, '/GripperMagnetOFF')
+
 	twist_msg = TwistStamped()
 	#twist_msg.header.frame_id = "auto"
 	
@@ -189,21 +192,32 @@ def main():
 					__twist_msg = TwistStamped()
 					__twist_msg.header.stamp = enftf.get_clock().now().to_msg()
 					__twist_msg.header.frame_id = ur5.base_link_name()
-					__twist_msg.twist.linear.z = 0.01
+					__twist_msg.twist.linear.z = 0.04
 					__twist_msg.twist.linear.x = -0.2
 					__twist_pub.publish(__twist_msg)
 				else:
 					break
+			
+			moveit2.move_to_pose(position=[-1, 0.12, 0.5], quat_xyzw=[box1.transform.rotation.x, box1.transform.rotation.y, -box1.transform.rotation.z, -box1.transform.rotation.w], cartesian=False,tolerance_position = 0.4,tolerance_orientation=0.4)
+			enftf.switch == True
+			moveit2.wait_until_executed()
+			moveit2.move_to_pose(position=[-0.8, 0.12, 0.5], quat_xyzw=[box1.transform.rotation.x, box1.transform.rotation.y, -box1.transform.rotation.z, -box1.transform.rotation.w], cartesian=False,tolerance_position = 0.4,tolerance_orientation=0.4)
+			moveit2.wait_until_executed()
+			while not gripper_control.wait_for_service(timeout_sec=1.0):
+				enftf.get_logger().info('EEF service not available, waiting again...')
+			print("tryiing to attach")
+			req = DetachLink.Request()
+			req.model1_name =  'box1'      
+			req.link1_name  = 'link'       
+			req.model2_name =  'ur5'       
+			req.link2_name  = 'wrist_3_link'
+			gripper_control_off.call_async(req)
 				
-			while rclpy.ok():
-				try:
-					moveit2.move_to_pose(position=[-0.37, 0.12, 0.397], quat_xyzw=[box1.transform.rotation.x, box1.transform.rotation.y, -box1.transform.rotation.z, -box1.transform.rotation.w], cartesian=False,tolerance_position = 0.3,tolerance_orientation=0.3)
-					moveit2.wait_until_executed()
-					enftf.switch == True
-				except:
-					pass
+			
+			rclpy.shutdown()
+			exit(0)
+				
 
-			break
 				# else:
 				#     rclpy.shutdown()
 				#     exit(0)
@@ -263,8 +277,7 @@ def main():
 
 		
 
-	rclpy.shutdown()
-	exit(0)
+	
 
 
 if __name__ == "__main__":
