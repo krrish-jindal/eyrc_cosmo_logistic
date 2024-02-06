4433876223bfa239bcfbc13db43c0b36e0503cd3
NOEMOJI041 <aloksharma041@gmail.com>
2024-02-06 13:15:54 +0530

    hardware test

---
+++ docking_server.py
@@ -0,0 +1,346 @@
+#!/usr/bin/env python3
+
+## Overview
+
+# ###
+# This ROS2 script is designed to control a robot's docking behavior with a rack. 
+# It utilizes odometry data, ultrasonic sensor readings, and provides docking control through a custom service. 
+# The script handles both linear and angular motion to achieve docking alignment and execution.
+# ###
+
+# Import necessary ROS2 packages and message types
+import rclpy
+from rclpy.node import Node
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Range
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+from tf_transformations import euler_from_quaternion
+from ebot_docking.srv import DockSw  # Import custom service message
+import math, statistics
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+import time
+from std_msgs.msg import Float32MultiArray
+
+# Define a class for your ROS2 node
+class MyRobotDockingController(Node):
+
+	def __init__(self):
+		# Initialize the ROS2 node with a unique name
+		super().__init__('my_robot_docking_controller')
+
+		# Create a callback group for managing callbacks
+		self.callback_group = ReentrantCallbackGroup()
+
+		self.get_logger().info("Server Started")
+
+
+		# Subscribe to odometry data for robot pose information
+		self.odom_sub = self.create_subscription(Odometry, 'odom', self.odometry_callback, 10)
+
+
+		self.ultra_sub = self.create_subscription(Float32MultiArray, 'ultrasonic_sensor_std_float', self.ultra_callback, 10)
+
+
+		# Create a ROS2 service for controlling docking behavior, can add another custom service message
+		self.dock_control_srv = self.create_service(DockSw, 'dock_control', self.dock_control_callback, callback_group=self.callback_group)
+
+		# Create a publisher for sending velocity commands to the robot
+		self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 10)
+
+		# Initialize all  flags and parameters here
+		self.is_docking = False
+		self.robot_pose = [0,0,0]         
+		self.dock_aligned = False
+		self.kp = 1.0
+		self.normalize_yaw_bot = 0
+		self.normalize_yaw_rack = 0
+		self.difference = 0
+		package_name = 'ebot_real_nav2'
+		config = "config/config.yaml"
+		self.flag =0
+
+
+		ebot_nav2_dir = get_package_share_directory('ebot_real_nav2')
+
+		pkg_share = FindPackageShare(package=package_name).find(package_name)
+		config_path = os.path.join(pkg_share, config)
+		with open(config_path, 'r') as infp:
+			pos_rack = infp.read()
+
+		self.x_pose = []
+		data_dict = yaml.safe_load(pos_rack)
+
+		positions = data_dict['position']
+		
+		self.rack1_coordinates = positions[0]['rack1']
+		self.x_pose.append(self.rack1_coordinates)
+		self.rack2_coordinates = positions[1]['rack2']
+		self.x_pose.append(self.rack2_coordinates)
+		self.rack3_coordinates = positions[2]['rack3']
+		self.x_pose.append(self.rack3_coordinates)
+
+		self.rack_yaw=[]
+
+		orientation_rack_1 = self.rack1_coordinates[2]
+		self.rack_yaw.append(orientation_rack_1)
+
+		orientation_rack_2 = self.rack2_coordinates[2]
+		self.rack_yaw.append(orientation_rack_2)
+
+		orientation_rack_3 = self.rack3_coordinates[2]
+		self.rack_yaw.append(orientation_rack_3)
+		# 
+		# 
+
+		# Initialize a timer for the main control loop
+		self.controller_timer = self.create_timer(0.1, self.controller_loop)
+
+	# Callback function for odometry data
+		
+	def odometry_callback(self, msg):
+		# Extract and update robot pose information from odometry message
+		self.robot_pose[0] = msg.pose.pose.position.x
+		self.robot_pose[1] = msg.pose.pose.position.y
+		quaternion_array = msg.pose.pose.orientation
+		orientation_list = [quaternion_array.x, quaternion_array.y, quaternion_array.z, quaternion_array.w]
+		_, _, yaw = euler_from_quaternion(orientation_list)
+		
+		self.robot_pose[2] = yaw
+
+	def ultra_callback(self,msg):
+		self.usrleft_value= msg.data[4]/100
+		self.usrright_value = msg.data[5]/100
+
+
+	# Utility function to normalize angles within the range of -π to π (OPTIONAL)
+	def normalize_angle(self, angle):
+		if angle < 0:
+			angle = math.pi + (math.pi + angle)
+		return angle
+
+	# Main control loop for managing docking behavior
+
+	def controller_loop(self):
+		vel = Twist()
+
+
+
+		# The controller loop manages the robot's linear and angular motion 
+		# control to achieve docking alignment and execution
+		if self.is_docking:
+  
+
+			# ...
+			# Implement control logic here for linear and angular motion
+			# For example P-controller is enough, what is P-controller go check it out !
+			# ...
+			#
+			self.difference = self.normalize_yaw_rack - self.normalize_yaw_bot
+
+			if self.orientation_dock ==True:
+				print("YAW------------YAW",(self.rack_yaw[int(self.rack_no)-1]))
+				print("LIST---------",self.rack_yaw,self.rack_no)
+
+
+				error = self.x_pose[int(self.rack_no) - 1][0] - self.robot_pose[0]
+				error2 = self.x_pose[int(self.rack_no) - 1][1] - self.robot_pose[1]
+				robot_head=str(self.robot_pose[2]/abs(self.robot_pose[2]))
+				print("ERRORS------",error,error2)
+
+# [0.9, 2.04, 0.0]
+
+
+
+##  X DIRECTION POSS CORRECTION
+				
+				if 0.3 > abs(self.x_pose[int(self.rack_no) - 1][0] - self.robot_pose[0]) > 0.025  and robot_head== "1.0":
+
+					print(self.robot_pose[0],self.robot_pose[1],"---------44444444",abs(self.x_pose[int(self.rack_no) - 1][0] - self.robot_pose[0]))
+					print("=============================================")
+
+					vel.linear.x = -error *0.4
+					self.vel_pub.publish(vel)
+
+				elif 0.3 > abs(self.x_pose[int(self.rack_no) - 1][0] - self.robot_pose[0]) > 0.025 and robot_head== "-1.0":
+					print(self.robot_pose[0],self.robot_pose[1],"---------44444444",abs(self.x_pose[int(self.rack_no) - 1][0] - self.robot_pose[0]))
+					vel.linear.x = error *0.4
+					self.vel_pub.publish(vel)
+
+
+
+#   Y DIRECTION POSS CORRECTION
+
+				elif 0.3 > abs(self.x_pose[int(self.rack_no) - 1][1] - self.robot_pose[1]) > 0.025  and robot_head== "1.0":
+					print(self.robot_pose[0],self.robot_pose[1],"---------33333333333",abs(self.x_pose[int(self.rack_no) - 1][1] - self.robot_pose[1]))
+					vel.linear.x = error2 *0.4
+					self.vel_pub.publish(vel)
+		
+				
+				elif 0.3 > abs(self.x_pose[int(self.rack_no) - 1][1] - self.robot_pose[1]) > 0.025  and robot_head== "-1.0":
+					print(self.robot_pose[0],self.robot_pose[1],"---------33333333333",abs(self.x_pose[int(self.rack_no) - 1][1] - self.robot_pose[1]))
+					vel.linear.x = -error2 *0.4
+					self.vel_pub.publish(vel)
+
+
+#   DESIRE POSS REACH 
+					
+				else:
+
+					print("Before")
+					vel.linear.x = 0.0
+					self.vel_pub.publish(vel)
+					self.flag = 1
+					self.orientation_dock = False
+
+
+
+# START ORIENTATION CORRECTION
+					
+			elif self.flag == 1:
+				if abs(self.difference) > 0.1:
+					vel.angular.z = self.difference *0.9
+					self.vel_pub.publish(vel)
+
+
+
+#  ORIENTATION CORRECTED
+					
+				else:
+					vel.angular.z = 0.0
+					self.vel_pub.publish(vel)
+					self.orientation_dock = False
+					self.linear_dock = False
+					self.flag = 0
+
+					print("successfully oriented",self.linear_dock)
+			
+
+
+#  LINEAR DOCKING STARTS
+					
+			elif self.linear_dock == False:
+				print("FLAG---",self.flag)
+				print("++++++++++++++++++++++++++++++++++++")
+				self.usrdiff=self.usrleft_value-self.usrright_value
+				# print("L-",self.usrleft_value,"R-",self.usrright_value)
+				# print("Diff---",self.difference)
+				self.diff=self.usrleft_value-self.usrright_value
+
+
+
+
+#   CORRECTING ULTRA SONIC DISTANCE ERROR
+				
+				# if self.usrleft_value > 0.15 and round(self.usrright_value,1) != round(self.usrleft_value,1):
+
+
+
+	#  CORRECTING ULRA SONIC ERROR 
+					
+					# if abs(self.difference)<=0.5:
+					# 	print(">>>>>=====>>>>>>")
+					# 	vel.angular.z = self.usrdiff*0.2 
+
+					# 	self.vel_pub.publish(vel)
+				
+	# CORRECTING RACK & BOT YAW ERROR
+
+					# else:
+					# 	print(">>>>>>>>>>>>>>>")
+					# 	vel.angular.z = self.difference*0.2
+					# 	vel.linear.x = -0.2
+
+					# 	self.vel_pub.publish(vel)
+
+ 
+					# # vel.linear.x = -self.usrleft_value * 0.4
+					# self.orientation_dock = False
+					# self.linear_dock = False
+
+
+
+#   NO ULTRA SONIC DISTANCE ERROR DIRECT DOCKING
+					
+				if  self.usrleft_value >= 0.14:
+					print("===============")
+					print(self.usrleft_value)
+					self.orientation_dock = False
+					vel.linear.x = -self.usrleft_value * 0.3
+					vel.angular.z = 0.0
+					self.vel_pub.publish(vel)
+					self.linear_dock = False
+
+
+
+#  DOCKING COMPLEATED
+					
+				else:
+					vel.linear.x = 0.0
+					self.vel_pub.publish(vel)
+					time.sleep(2)
+					print("docking done")
+					self.linear_dock = True
+					self.is_docking = False
+					self.dock_aligned = True
+				
+
+	# Callback function for the DockControl service
+	def dock_control_callback(self, request, response):
+		# Extract desired docking parameters from the service request
+		self.linear_dock = request.linear_dock
+		self.orientation_dock = request.orientation_dock
+		self.distance = request.distance
+		self.orientation = request.orientation
+		self.rack_no = request.rack_no
+		# print(self.orientation)
+
+		self.normalize_yaw_rack = self.normalize_angle(self.orientation)
+		# print(self.normalize_yaw_rack)
+		self.normalize_yaw_bot = self.normalize_angle(self.robot_pose[2])
+		# print(self.normalize_yaw_bot)
+
+
+		# Reset flags and start the docking process
+		self.dock_aligned = False
+		print(self.dock_aligned)
+
+		# Log a message indicating that docking has started
+		self.get_logger().info("Docking started!")
+		# Create a rate object to control the loop frequency
+		self.rate = self.create_rate(2, self.get_clock())
+
+		# Wait until the robot is aligned for docking
+		while not self.dock_aligned:
+			self.normalize_yaw_rack = self.normalize_angle(self.orientation)
+			self.normalize_yaw_bot = self.normalize_angle(self.robot_pose[2])
+			self.is_docking = True
+			self.get_logger().info("Waiting for alignment...")
+			self.controller_loop()
+			self.rate.sleep()
+
+		# Set the service response indicating success
+		response.success = True
+		response.message = "Docking control initiated"
+		return response
+
+# Main function to initialize the ROS2 node and spin the executor
+def main(args=None):
+	rclpy.init(args=args)
+
+	my_robot_docking_controller = MyRobotDockingController()
+
+	executor = MultiThreadedExecutor()
+	executor.add_node(my_robot_docking_controller)
+
+	executor.spin()
+
+	my_robot_docking_controller.destroy_node()
+	rclpy.shutdown()
+
+if __name__ == '__main__':
+	main()
---
+++ movit.py
@@ -0,0 +1,404 @@
+#!/usr/bin/env python3
+
+from os import path
+from threading import Thread
+
+import rclpy
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.node import Node
+import time
+from pymoveit2 import MoveIt2
+from pymoveit2.robots import ur5
+from arm_picky.srv import ArmNew
+import math
+import sys
+import tf2_ros
+from rclpy.node import Node
+from geometry_msgs.msg import TransformStamped
+from scipy.spatial.transform import Rotation as R
+from builtin_interfaces.msg import Time
+from geometry_msgs.msg import Quaternion
+from tf_transformations import quaternion_from_euler
+from tf_transformations import euler_from_quaternion
+import tf_transformations
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import TwistStamped
+
+from math import cos, sin
+import math, time
+from copy import deepcopy
+import rclpy
+import tf2_ros
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.node import Node
+from geometry_msgs.msg import TwistStamped
+from pymoveit2.robots import ur5
+from rclpy.qos import (
+	QoSDurabilityPolicy,
+	QoSHistoryPolicy,
+	QoSProfile,
+	QoSReliabilityPolicy,
+)
+
+from ur_msgs.srv import SetIO
+from controller_manager_msgs.srv import SwitchController # module call
+
+endf_x = 0
+endf_y = 0
+endf_z = 0
+
+
+pos1 = [0.35, 0.1, 0.68]
+pos2 = [0.35, 0.1, 0.68]
+pos3 = [0.35, 0.1, 0.68]
+
+def truncate(f, n):
+	return math.floor(f * 10 ** n) / 10 ** n
+class endf(Node):
+    def __init__(self):
+        super().__init__('move_ur5') 
+        self.callback_group = ReentrantCallbackGroup()
+        self.tf_buffer = tf2_ros.buffer.Buffer()                                        # buffer time used for listening transforms
+        self.listener = tf2_ros.TransformListener(self.tf_buffer, self)
+        self.br = tf2_ros.TransformBroadcaster(self)
+        self.switch = False
+        self.arm_srv = self.create_service(ArmNew, 'arm_control', self.arm_control_callback, callback_group=self.callback_group)
+        self.__contolMSwitch = self.create_client(SwitchController, "/controller_manager/switch_controller")
+        self.tf_topic = self.create_subscription(TFMessage, '/tf', self.tf_cb , 10)
+        self.twist_pub = self.create_publisher(TwistStamped, "/servo_node/delta_twist_cmds", 10)
+        self.pz = 0
+        self.obj_aruco = "None"
+        self.last_obj = "None"
+
+        self.moveit2 = MoveIt2(
+        node=self,
+        joint_names=ur5.joint_names(),
+        base_link_name=ur5.base_link_name(),
+        end_effector_name=ur5.end_effector_name(),
+        group_name=ur5.MOVE_GROUP_ARM,
+        callback_group=self.callback_group,)
+        self.declare_parameter(
+        "joint_positions_initial",
+        [
+            0.0,
+            -2.39,
+            2.4,
+            -3.15,
+            -1.58,
+            3.15
+        ],
+    )
+        self.declare_parameter(
+        "joint_positions_positive",
+        [
+            1.57,
+            -2.39,
+            2.4,
+            -3.15,
+            -1.58,
+            3.15
+        ],
+    )
+        self.declare_parameter(
+        "joint_positions_negative",
+        [
+            -1.57,
+            -2.39,
+            2.4,
+            -3.15,
+            -1.58,
+            3.15
+        ],
+    )
+        self.declare_parameter(
+            "joint_positions_back",
+            [
+                -2.96706,
+                -1.6057,
+                1.67552,
+                -3.14159,
+                -1.58825,
+                3.14159
+            ],
+        )
+        self.declare_parameter(
+        "joint_positions_final_1",
+        [
+            -0.027,
+            -1.803,
+            -1.3658,
+            -3.039,
+            -1.52,
+            3.15
+        ],
+    )
+        self.declare_parameter(
+        "joint_positions_final_2",
+        [
+            -0.279,
+            -2.827,
+            -0.244,
+            -3.14159,
+            -1.58825,
+            3.14159
+        ],
+    )
+        self.declare_parameter(
+        "joint_positions_final_3",
+        [
+            -2.967,
+            -0.715,
+            0.89,
+            -3.14159,
+            -1.58825,
+            3.14159
+        ],
+    )
+
+    def arm_control_callback(self, request, response):
+        print("I am here")
+        self.variable_a = request.boom  
+        self.variable_b = request.whack
+        response.reply = True
+        return response
+    
+    def trajactory(self):
+        switchParam = SwitchController.Request()
+        switchParam.activate_controllers = ["scaled_joint_trajectory_controller"]# for normal use of moveit
+        switchParam.deactivate_controllers = ["forward_position_controller"]
+        switchParam.strictness = 2
+        switchParam.start_asap = False
+
+        while not self.__contolMSwitch.wait_for_service(timeout_sec=5.0):
+            self.get_logger().warn(f"Service control Manager is not yet available...")
+        self.__contolMSwitch.call_async(switchParam)
+        print("[CM]: Switching Complete Trajec")
+
+    def servo_active(self):
+        switchParam = SwitchController.Request()
+        switchParam.deactivate_controllers = ["scaled_joint_trajectory_controller"]
+        switchParam.activate_controllers = ["forward_position_controller"] # for servoing
+        switchParam.strictness = 2
+        switchParam.start_asap = False
+
+        while not self.__contolMSwitch.wait_for_service(timeout_sec=5.0):
+            self.get_logger().warn(f"Service control Manager is not yet available...")
+        self.__contolMSwitch.call_async(switchParam)
+        print("[CM]: Switching Complete Servo")
+
+        while not self.__contolMSwitch.wait_for_service(timeout_sec=5.0):
+            self.get_logger().warn(f"Service control Manager is not yet available...")
+        self.__contolMSwitch.call_async(switchParam)
+        print("[CM]: Switching Complete")
+
+    def gripper_call(self, state):
+        '''
+        based on the state given as i/p the service is called to activate/deactivate
+        pin 16 of TCP in UR5
+        i/p: node, state of pin:Bool
+        o/p or return: response from service call
+        '''
+        gripper_control = self.create_client(SetIO, '/io_and_status_controller/set_io')
+        while not gripper_control.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('EEF Tool service not available, waiting again...')
+        req         = SetIO.Request()
+        req.fun     = 1
+        req.pin     = 16
+        req.state   = float(state)
+        gripper_control.call_async(req)
+        return state
+    
+    def tf_cb(self, data):
+        self.obj = "None"
+        try:
+            if data.transforms[0].header.frame_id == "base_link":
+                if "obj" in data.transforms[0].child_frame_id:
+                    self.obj_aruco = data.transforms[0].child_frame_id
+        except Exception as e:
+            pass
+
+    def servo(self, box_no):
+        while rclpy.ok():
+            try:
+                tool0 = self.tf_buffer.lookup_transform('base_link', "tool0", rclpy.time.Time())
+                box49 = self.tf_buffer.lookup_transform('base_link', f"obj_{box_no}", rclpy.time.Time())
+
+                while rclpy.ok():
+                    try:
+                        box49 = self.tf_buffer.lookup_transform('base_link', f"obj_{box_no}", rclpy.time.Time())
+                        tool0 = self.tf_buffer.lookup_transform('base_link', "tool0", rclpy.time.Time())
+                    except Exception as e:
+                        pass
+                    print(f"y ={round((box49.transform.translation.y) - (tool0.transform.translation.y),4)}")
+                    print(f"x ={round((box49.transform.translation.x) - (tool0.transform.translation.x),4)}")
+                    print(f"z ={round((box49.transform.translation.z) - (tool0.transform.translation.z),4)}")
+                    joint_positions_initial = (self.get_parameter("joint_positions_initial").get_parameter_value().double_array_value)
+                    joint_positions_final_1 = (self.get_parameter("joint_positions_final_1").get_parameter_value().double_array_value)
+                    joint_positions_final_2 = (self.get_parameter("joint_positions_final_2").get_parameter_value().double_array_value)
+                    joint_positions_final_3 = (self.get_parameter("joint_positions_final_3").get_parameter_value().double_array_value)
+                    joint_positions_back = (self.get_parameter("joint_positions_back").get_parameter_value().double_array_value)
+
+
+ #  ARM Move To BOX 
+
+                    if round((box49.transform.translation.y) - (tool0.transform.translation.y),4) > 0.004 and round((box49.transform.translation.x) - (tool0.transform.translation.x),4) > 0.004 and round((box49.transform.translation.z) - (tool0.transform.translation.z),4) > 0.004:
+                        __twist_msg = TwistStamped()
+                        __twist_msg.header.stamp = self.get_clock().now().to_msg()
+                        __twist_msg.header.frame_id = ur5.base_link_name()
+                        __twist_msg.twist.linear.y = round((box49.transform.translation.y) - (tool0.transform.translation.y),4) *2
+                        __twist_msg.twist.linear.x = round((box49.transform.translation.x) - (tool0.transform.translation.x),4) *2
+                        __twist_msg.twist.linear.z = round((box49.transform.translation.z) - (tool0.transform.translation.z),4) *2
+                        self.twist_pub.publish(__twist_msg)
+                        print("SERVO_START")
+
+#   Gripper ON
+                    else :
+                        print("trying to attach")
+                        self.gripper_call(1)
+                        print("DONE")
+                        break
+
+                while rclpy.ok():
+                    try:
+                        box49 = self.tf_buffer.lookup_transform('base_link', f"obj_{box_no}", rclpy.time.Time())
+                        tool0 = self.tf_buffer.lookup_transform('base_link', "tool0", rclpy.time.Time())
+                        roll , pitch , yaw  = euler_from_quaternion([box49.transform.rotation.x, box49.transform.rotation.y, box49.transform.rotation.z, box49.transform.rotation.w])
+
+
+                    except Exception as e:
+                        pass
+
+
+#   ARM Adding Buffer POSS  &  Drop Location
+                        
+                    if ((round(yaw,2) >= 2.79 and round(yaw,2)<= 3.14) ) and (round(tool0.transform.translation.y,2) > 0.25):
+                        
+                        __twist_msg = TwistStamped()
+                        __twist_msg.header.stamp = self.get_clock().now().to_msg()
+                        __twist_msg.header.frame_id = ur5.base_link_name()
+                        __twist_msg.twist.linear.z = 0.1
+                        __twist_msg.twist.linear.y = -0.2
+                        self.twist_pub.publish(__twist_msg)
+                    elif (round(yaw,2) >= 2.79 and round(yaw,2)<= 3.14) and (round(tool0.transform.translation.y,2) <= 0.25):
+                        self.trajactory()
+                        self.moveit2.move_to_configuration(joint_positions_initial)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_initial)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_final_2)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_final_2)
+                        self.moveit2.wait_until_executed()
+                        break 
+
+                    elif (round(yaw,2) >= 1.39 and round(yaw,2)<=1.91) and (round(tool0.transform.translation.x,2) > 0.23):
+                        __twist_msg = TwistStamped()
+                        __twist_msg.header.stamp = self.get_clock().now().to_msg()
+                        __twist_msg.header.frame_id = ur5.base_link_name()
+                        __twist_msg.twist.linear.z = 0.1
+                        __twist_msg.twist.linear.x = -0.4
+                        self.twist_pub.publish(__twist_msg)
+                    elif (round(yaw,2) >= 1.39 and round(yaw,2)<=1.91) and (round(tool0.transform.translation.x,2) <= 0.23):
+                        self.trajactory()
+                        self.moveit2.move_to_configuration(joint_positions_final_1)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_final_1)
+                        self.moveit2.wait_until_executed()
+                        break
+                    
+                    elif round(yaw,2) >= -0.17 and round(yaw,2) <=0.52 and (round(tool0.transform.translation.y,2) < -0.26):
+                        __twist_msg = TwistStamped()
+                        __twist_msg.header.stamp = self.get_clock().now().to_msg()
+                        __twist_msg.header.frame_id = ur5.base_link_name()
+                        __twist_msg.twist.linear.z = 0.04
+                        __twist_msg.twist.linear.y = 0.2
+                        __twist_msg.twist.linear.x = -0.2
+                        self.twist_pub.publish(__twist_msg)
+
+                    elif round(yaw,2) >= -0.17 and round(yaw,2) <=0.52 and (round(tool0.transform.translation.y,2) >= -0.26):
+                        self.trajactory()
+                        self.moveit2.move_to_configuration(joint_positions_back)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_back)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_final_3)
+                        self.moveit2.wait_until_executed()
+                        self.moveit2.move_to_configuration(joint_positions_final_3)
+                        self.moveit2.wait_until_executed()
+                        break
+
+
+
+                   
+
+
+
+#    Gripper OFF
+
+                print("trying to detach")
+                self.gripper_call(0)
+                break
+                    
+            except Exception as e:
+                print("Servo Function 2nd Part")
+
+
+def main():
+
+    rclpy.init()
+    enftf = endf()
+
+    executor = rclpy.executors.MultiThreadedExecutor(2)
+    executor.add_node(enftf)
+    executor_thread = Thread(target=executor.spin, daemon=True, args=())
+    executor_thread.start()
+
+    joint_positions_initial = (enftf.get_parameter("joint_positions_initial").get_parameter_value().double_array_value)
+    joint_positions_positive = (enftf.get_parameter("joint_positions_positive").get_parameter_value().double_array_value)
+    joint_positions_negative = (enftf.get_parameter("joint_positions_negative").get_parameter_value().double_array_value)
+    joint_positions_back = (enftf.get_parameter("joint_positions_back").get_parameter_value().double_array_value)
+
+
+
+    while rclpy.ok():
+        try:
+            obj = enftf.obj_aruco[4:]
+            print(obj)
+            box = enftf.tf_buffer.lookup_transform('base_link', enftf.obj_aruco, rclpy.time.Time())
+            roll , pitch , yaw  = euler_from_quaternion([box.transform.rotation.x, box.transform.rotation.y, box.transform.rotation.z, box.transform.rotation.w])
+            print(round(yaw,2))
+            if enftf.variable_a == True:
+                print("You are my special")
+                if round(yaw,2) >= -0.17 and round(yaw,2) <=0.52:
+                    enftf.trajactory()
+                    enftf.moveit2.move_to_configuration(joint_positions_negative)
+                    enftf.moveit2.wait_until_executed()
+                    enftf.servo_active()
+                    enftf.servo(obj)
+                    enftf.trajactory()
+                    enftf.moveit2.move_to_configuration(joint_positions_back)
+                    enftf.moveit2.wait_until_executed()
+                    enftf.moveit2.move_to_configuration(joint_positions_initial)
+                    enftf.moveit2.wait_until_executed()
+
+                elif (round(yaw,2) >= 2.79 and round(yaw,2)<= 3.14) :
+                    enftf.trajactory()
+                    enftf.moveit2.move_to_configuration(joint_positions_positive)
+                    enftf.moveit2.wait_until_executed()
+                    enftf.servo_active()
+                    enftf.servo(obj)
+                    enftf.trajactory()
+                    enftf.moveit2.move_to_configuration(joint_positions_initial)
+                    enftf.moveit2.wait_until_executed()
+
+                elif round(yaw,2) >= 1.39 and round(yaw,2)<=1.91:
+                    enftf.servo_active()
+                    enftf.servo(obj)
+                    enftf.trajactory()
+                    enftf.moveit2.move_to_configuration(joint_positions_initial)
+                    enftf.moveit2.wait_until_executed()
+        except Exception as e:
+            print("Main Function")
+
+if __name__ == "__main__":
+	main()
